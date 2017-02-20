e144dfb88e0a91ad91afac1b8234fc9f4075f580
Luis Gaytan <gaytan_luis@msn.com>
2017-02-19 22:23:44 -0800

    style about page & link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,21 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to About Page</p>
+      <h2 className="text-center">About</h2>
+      <p>This is a React web application that helps get the weather information.</p>
+      <p>The following are resrouces that I used in creating this application:
+        <ul>
+          <li>
+            <a href="https://facebook.github.io/react" target="_blank">React</a> &mdash; This was the JavaScript framework used.
+          </li>
+          <li>
+            <a href="http://openweathermap.org" target="_blank">Open Weather Map</a> &mdash; I used this API to search for weather data by city name.
+          </li>
+          <li>
+            <a href="http://techiedodo.com" target="_blank">Techie Dodo</a> &mdash; This is my web page. Feel free to browse and learn a bit more about me.
+          </li>
+        </ul>
+      </p>
     </div>
   )
 };
